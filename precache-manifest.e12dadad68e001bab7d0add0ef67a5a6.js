020b64930acdffb5848c9907dbbb7cb65f5f6de2
RoshRoslin <RoshRoslin@gmail.com>
2021-05-07 12:47:18 -0700

    Updates

---
+++ precache-manifest.e12dadad68e001bab7d0add0ef67a5a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc38f5f03bb6f6853ef258635da159a",
+    "url": "/tours-react-project/index.html"
+  },
+  {
+    "revision": "13a2f8c9392c585bea14",
+    "url": "/tours-react-project/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/tours-react-project/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tours-react-project/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a2f8c9392c585bea14",
+    "url": "/tours-react-project/static/js/main.546159e2.chunk.js"
+  },
+  {
+    "revision": "bdda2e3d183933d90c02",
+    "url": "/tours-react-project/static/js/runtime-main.2bdfe9c9.js"
+  }
+]);
